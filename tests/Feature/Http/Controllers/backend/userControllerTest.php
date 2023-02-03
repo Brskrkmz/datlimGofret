da2fc1c71bb36a24aa95a05749e7105c64ca9926
Barış <brskrkmzphp@gmail.com>
2023-02-03 14:35:09 +0300

    17. Commit

--- tests/Feature/Http/Controllers/backend/userControllerTest.php
+++ tests/Feature/Http/Controllers/backend/userControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Http\Controllers\backend;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class userControllerTest extends TestCase
 {
