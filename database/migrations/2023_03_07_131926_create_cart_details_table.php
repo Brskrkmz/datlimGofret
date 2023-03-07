134782658888c53af7b1619e26cab183a1c3a004
Barış <brskrkmzphp@gmail.com>
2023-03-07 16:26:05 +0300

    29. commit

---
+++ database/migrations/2023_03_07_131926_create_cart_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\product;
+use App\Models\cart;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_details', function (Blueprint $table) {
+            $table->id('cart_detail_id');
+            $table->foreignIdFor(cart::class,'cart_id');
+            $table->foreignIdFor(product::class,'product_id');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_details');
+    }
+};
