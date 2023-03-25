adb99031f9eaf04f03b103a15e4ba2f3b3df0e2c
Barış <brskrkmzphp@gmail.com>
2023-03-25 13:27:29 +0300

    34. commit

--- app/Http/Controllers/frontend/authController.php
+++ app/Http/Controllers/frontend/authController.php
@@ -9,9 +9,28 @@ class authController extends Controller
 {
     public function sinInForm():View
     {
-        return view(frontend);
+        return view("frontend.auth.singIn_form");
     }
-    public function sinIn(){}
-    public function sinUpForm(){}
-    public function sinUp(){}
+    public function sinIn(sinInRequest $request)
+    {
+        $credentials = $request->only(["email", "password"]);
+        $rememberMe = $request->get("remember-me", false);
+
+        if(Auth::attempt($credentials, $rememberMe)){
+            return redirect("/");
+        }else{
+            return redirect()->back()->withErrors([
+                "email" => 'Lütfen parolanızı ve şifrenizi kontrol ederek tekrar giriniz.',
+                "password" => 'Lütfen parolanızı ve şifrenizi kontrol ederek tekrar giriniz.'
+            ]);
+        }
+    }
+    public function sinUpForm()
+    {
+        return view("frontend.auth.singUp_form");
+    }
+    public function sinUp()
+    {
+        
+    }
 }
